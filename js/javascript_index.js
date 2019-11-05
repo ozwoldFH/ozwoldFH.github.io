a6404fa568aaed158d9c7c41e9ba6b52f001e1cc
George Oswald <george.oswald@edu.fh-joanneum.at>
2019-11-05 21:43:06 +0100

    added id for table. updated javascript_index.js to fill table (dynamic)

--- js/javascript_index.js
+++ js/javascript_index.js
@@ -1,25 +1,42 @@
 "use strict";
-window.addEventListener("load", function(){
+
+window.addEventListener("load", function(){    
     console.log("=== page ready ===");
+
+    // member variables
+    var tableOutput = document.getElementById("tableOutput");
+    var dataJSON = "[ ]";
 
     function ajaxLoadData(){
         console.log(this); 
-
         if(this.readyState == 4 && this.status == 200) {
-            var dataJSON = JSON.parse(this.responseText);
+            dataJSON = JSON.parse(this.responseText);
+            createHeader();
+            createBody();            
+        }
+        else {
+            console.log("Error! JSON Message read failed!");
+        }
 
-            console.log(dataJSON);
-            for (var row in dataJSON) {
-                for(var column in dataJSON[row]) {
-                    console.log(dataJSON[row][column]);
-                }
+    }
+
+    function createHeader() {
+        var rowHeader = tableOutput.insertRow();
+        for(var column in dataJSON[0]) {
+            var columnHeader = document.createElement("th");
+            columnHeader.innerHTML = column;
+            rowHeader.appendChild(columnHeader);
+        }
+    }
+
+    function createBody() {
+        for (var row in dataJSON) {
+            var rowBody = tableOutput.insertRow();
+            for(var column in dataJSON[row]) {
+                var columnBody = document.createElement("td");
+                columnBody.innerHTML = dataJSON[row][column];
+                rowBody.appendChild(columnBody);
             }
-      
-          
-
-
-
-            
         }
     }
 
@@ -31,6 +48,9 @@ window.addEventListener("load", function(){
         ajaxObject.send();
     }
 
-    loadData()
+
+
+    // call function
+    loadData();
 
 });
