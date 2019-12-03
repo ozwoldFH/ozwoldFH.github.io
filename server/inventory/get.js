ed23b226d380065ee80af7933fbc655db1f8d5d2
Clemens Ott <clemens.ott@edu.fh-joanneum.at>
2019-12-03 11:12:36 +0100

    fixed search

--- server/inventory/get.js
+++ server/inventory/get.js
@@ -7,17 +7,17 @@ async function get() {
     return result.map(item => {
         return {
             id: item.id,
-            name: item.item_name,
-            weightKg: item.weight,
-            description: item.description,
-            location: item.location,
-            room: item.room,
-            type: item.item_type,
-            addedDateTime: item.added_datetime,
-            addedBy: item.added_by,
-            lastServiceDateTime: item.last_service_datetime,
-            lastServiceBy: item.last_service_by,
-            nextServiceDateTime: item.next_service_datetime,
+            name: item.item_name || '',
+            weightKg: item.weight || 0,
+            description: item.description || '',
+            location: item.location || '',
+            room: item.room || '',
+            type: item.item_type || '',
+            addedDateTime: item.added_datetime || '',
+            addedBy: item.added_by || '',
+            lastServiceDateTime: item.last_service_datetime || '',
+            lastServiceBy: item.last_service_by || '',
+            nextServiceDateTime: item.next_service_datetime || '',
         };
     });
 }
