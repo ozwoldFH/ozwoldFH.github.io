fa58490e6ce271c852cb5b97dc577a2ac5deabef
Clemens Ott <clemens.ott@edu.fh-joanneum.at>
2019-12-03 12:35:50 +0100

    added fontawesome

--- server/helper/sendStaticFiles.js
+++ server/helper/sendStaticFiles.js
@@ -1,21 +1,48 @@
 const path = require('path');
 const readFile = require('./readFile');
+const fs = require('fs');
 
 async function send(relativePath, response) {
-    const contentType = getContentType(path.extname(relativePath));
+    const extname = path.extname(relativePath);
+    const contentType = getContentType(extname);
 
-    try {
-        const content = await readFile(relativePath);
-        response.writeHead(200, {'Content-Type': contentType});
-        response.end(content, 'utf-8');
-    } catch (err) {
-        response.writeHead(err.code, {'Content-Type': contentType});
-        response.end(err.message, 'utf-8');
+    if (contentType) {
+        try {
+            const content = await readFile(relativePath);
+            response.writeHead(200, {'Content-Type': contentType});
+            response.end(content, 'utf-8');
+        } catch (err) {
+            response.writeHead(err.code, {'Content-Type': contentType});
+            response.end(err.message, 'utf-8');
+        }
+    } else {
+        try {
+            const stat = fs.statSync(relativePath);
+            response.writeHead(200, {
+                'Content-Type': 'file/' + extname.slice(1),
+                'Content-Length': stat.size
+            });
+
+            const readStream = fs.createReadStream(relativePath);
+            // We replaced all the event handlers with a simple call to readStream.pipe()
+            readStream.pipe(response);
+        } catch (err) {
+            if (err.code === 'ENOENT') {
+                response.writeHead(404, {'Content-Type': 'text/plain'});
+                response.end('File not found', 'utf-8');
+            } else {
+                response.writeHead(500, {'Content-Type': 'text/plain'});
+                response.end('Sorry, check with the site admin for error: ' + err.code, 'utf-8');
+            }
+        }
     }
 }
 
 function getContentType(extname) {
     switch (extname) {
+        case '.html':
+            return 'text/html';
+            break;
         case '.js':
             return 'text/javascript';
             break;
@@ -31,12 +58,16 @@ function getContentType(extname) {
         case '.jpg':
             return 'image/jpg';
             break;
+        case '.ico':
+            return null;
+            return 'image/ico';
+            break;
         case '.wav':
             return 'audio/wav';
             break;
     }
 
-    return 'text/html';
+    return null;
 }
 
 module.exports = send;
