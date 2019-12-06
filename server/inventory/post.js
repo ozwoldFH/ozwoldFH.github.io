9e04d34add063524ebdd9213be3bfac525a2582c
George Oswald <george.oswald@edu.fh-joanneum.at>
2019-12-06 21:17:05 +0100

    fixed date bug where empty string for datetime throws an error

--- server/inventory/post.js
+++ server/inventory/post.js
@@ -2,6 +2,12 @@ const dbQuery = require('../helper/db');
 
 async function post({ name, weight, description, location, room, type, addedDateTime, addedBy, lastServiceDateTime, lastServiceBy, nextServiceDateTime }) {
     try {
+        if (lastServiceDateTime === '') {
+            lastServiceDateTime = null;
+        }
+        if (nextServiceDateTime === '') {
+            nextServiceDateTime = null;
+        }
         const sql = {
             text: `INSERT INTO T_INVENTORY (item_name,weight,description,location,room,item_type,added_datetime,added_by,last_service_datetime,last_service_by,next_service_datetime)
                     VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`,
