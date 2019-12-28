6f95e52234c81021abde4b343b6f544f542fd244
Clemens Ott <clemens.ott@edu.fh-joanneum.at>
2019-12-28 08:28:59 +0100

    open and closes db connection on every db call

--- server/helper/db.js
+++ server/helper/db.js
@@ -1,35 +1,23 @@
 const {Client} = require('pg');
 
-const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
-});
-let connectPromise;
-
 async function getClient() {
-    if (!connectPromise) {
-        connectPromise = client.connect();
-    }
-
-    await connectPromise;
+    const client = new Client({
+        connectionString: process.env.DATABASE_URL,
+        ssl: true,
+    });
+    await client.connect();
     return client;
 }
 
 async function query(sql) {
     const client = await getClient();
-    return new Promise((resolve, reject) => {
-        client.query(sql, (err, res) => {
-            if (err) {
-                reject(err);
-            } else {
-                if (res.rows.length) {
-                    resolve([...res.rows]);
-                } else {
-                    resolve(res.rowCount);
-                }
-            }
-        });
-    });
+    const res = await client.query(sql);
+    await client.end();
+    if (res.rows.length) {
+        return [...res.rows];
+    } else {
+        return res.rowCount;
+    }
 }
 
 module.exports = query;
